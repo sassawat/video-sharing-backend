b7e3ac92607a3a1371aec2d8fab362c1d1078fce
Sassawat Suk-udom <tong_sasawat@hotmail.com>
2020-06-22 21:47:29 +0700

    Update config to db on prod

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 # Config to connect database
 CONFIG_DB = {
-    "user": "root", 
-    "password": "123456", 
-    "host": "10.0.253.241", 
-    "port": 9988, 
+    "user": "kodadmin", 
+    "password": "SDD.kod@!t2020", 
+    "host": "10.214.33.232", 
+    "port": 3306, 
     "database": "video_sharing"
 }
 
 # Path video for upload store in server
 UPLOAD_FOLDER = 'src/asset/video/'
+# UPLOAD_FOLDER = 'C:/Users/tong_/Documents/Github/video-sharing-web-backend_1/src/asset/video'
 
